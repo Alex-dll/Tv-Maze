51f30e6ce13f5577bab0a28ff86d4dd8ee314b61
alex-dll <alexsandrojr.303@gmail.com>
2022-09-18 19:43:54 -0300

    test: :white_check_mark: Tests to StarRating

--- src/components/StarRating/__tests__/StarRating.test.tsx
+++ src/components/StarRating/__tests__/StarRating.test.tsx
@@ -3,13 +3,35 @@ import {render} from '@testing-library/react-native';
 import {StarRating} from '../StarRating';
 
 describe('StarRating', () => {
-  it('the component rendered', () => {
-    render(
-      <StarRating
-        rating={{
-          average: 7,
-        }}
-      />,
-    );
+  describe('rating was passed', () => {
+    it('show the average', () => {
+      const {getByText} = render(
+        <StarRating
+          rating={{
+            average: 7,
+          }}
+        />,
+      );
+      expect(getByText('7')).toBeTruthy();
+    });
+
+    it('show the star icon', () => {
+      const {getByTestId} = render(
+        <StarRating
+          rating={{
+            average: 7,
+          }}
+        />,
+      );
+      expect(getByTestId('starIcon')).toBeTruthy();
+    });
+  });
+
+  describe('rating was NOT passing', () => {
+    it('return nothing', () => {
+      const {container} = render(<StarRating />);
+
+      expect(container.children.length).toEqual(0);
+    });
   });
 });
