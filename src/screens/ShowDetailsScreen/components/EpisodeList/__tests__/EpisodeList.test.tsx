0324244b83a9afbf4f91f9ab8bae5060c1361c65
alex-dll <alexsandrojr.303@gmail.com>
2022-09-18 22:46:01 -0300

    test: add tests to EpisodeList

---
+++ src/screens/ShowDetailsScreen/components/EpisodeList/__tests__/EpisodeList.test.tsx
@@ -0,0 +1,46 @@
+import React, {ReactChildren} from 'react';
+import {render} from '@testing-library/react-native';
+
+import {EpisodeList} from '../EpisodeList';
+import {mocks} from './mocks';
+import {QueryClient, QueryClientProvider} from 'react-query';
+import {showService} from '../../../../../services/show/showService';
+
+type wrapperProps = {
+  children: ReactChildren;
+};
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: Infinity,
+    },
+  },
+});
+
+const wrapper = ({children}: wrapperProps) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
+
+describe('EpisodeList', () => {
+  test('show all season one episodes at first', async () => {
+    jest.spyOn(showService, 'getEpisodes').mockResolvedValueOnce({
+      seasonNames: ['1', '2'],
+      seasons: {
+        1: [mocks.episode1, mocks.episode2],
+        2: [mocks.episode22, mocks.episode23],
+      },
+    });
+    const {getByText, findByText} = render(<EpisodeList show={mocks.show} />, {
+      wrapper,
+    });
+
+    // await waitFor(() => {
+    //   getByText(mocks.episode1.name);
+    // });
+    await findByText(mocks.episode1.name);
+
+    expect(getByText(mocks.episode1.name)).toBeTruthy();
+    expect(getByText(mocks.episode2.name)).toBeTruthy();
+  });
+});
